ff92d88df206f62c668a71efe84f96f01874d8b1
Joe Kearney <jjk@soundcloud.com>
2015-10-08 16:48:00 +0200

    add gitprompt to linker

--- link.sh
+++ link.sh
@@ -2,6 +2,6 @@
 
 DOT_FILE_DIR=$(readlink -f $0 | xargs dirname)
 
-for f in .bashrc .bash_profile .vimrc; do
+for f in .bashrc .bash_profile .vimrc .git-prompt.sh; do
 	ln -sf $DOT_FILE_DIR/$f ~/$(basename $f)
 done
