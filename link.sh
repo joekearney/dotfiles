bc9b79ad78f1829c30f569542dd8587cd49dc310
Joe Kearney <jjk@soundcloud.com>
2015-10-27 11:59:02 +0100

    add git config

--- link.sh
+++ link.sh
@@ -2,6 +2,8 @@
 
 DOT_FILE_DIR=$(readlink -f $0 | xargs dirname)
 
-for f in .bashrc .bash_profile .vimrc .git-prompt.sh; do
+for f in .bashrc .bash_profile .vimrc .git-prompt.sh .gitconfig; do
 	ln -sf $DOT_FILE_DIR/$f ~/$(basename $f)
 done
+
+echo "If you need to commit to git, remember to update your email in $HOME/.config/git/config, in a [user].email property."
