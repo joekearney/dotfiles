b468de7ccd1ded6fe2454d4ef8d1545219426a7a
Joe Kearney <jjk@soundcloud.com>
2015-10-30 15:57:02 +0100

    bash auto complete for host names

--- functions.sh
+++ functions.sh
@@ -5,6 +5,7 @@ function runCommand() {
     "$@"
   fi
 }
+# Pushes the dotfiles directory to the target machine and links them
 function pushDotFilesTo() {
   if [[ "$1" == "" ]]; then
     echo "Usage: pushDotFilesTo <host>"
@@ -13,10 +14,13 @@ function pushDotFilesTo() {
     ${DOT_FILES_DIR}/push_rc.sh $host
   fi
 }
+
+# Sources .bash_profile into the current environment
 function reloadDotFiles() {
   . ~/.bash_profile
 }
 
+# sleeps for a number of seconds, displaying the number of seconds remaining.
 function countdown() {
   local remaining=$1
   while ((remaining>0)); do
@@ -26,6 +30,7 @@ function countdown() {
   done
 }
 
+# sets the shell title
 function shellTitle() {
     echo -ne "\033]0;"$*"\007"
 }
@@ -35,10 +40,14 @@ function hag() {
   sed -e '1p' -e "/$1/!d"
 }
 
+export KNOWN_HOSTS_FILE=$DOT_FILES_DIR/.known_hosts
 # ssh, but passing an LC environment variable with the expected target host name.
 # This is so that you can ssh to some alias, that lands you on some target
 # machine of a cluster, and be able to find out later what you though the host
 # was called.
+#
+# If there are more arguments then we just delegate to the default ssh, and
+# don't get the hostname passed through.
 function ash() {
   if [[ "$1" == "" ]]; then
     # delegate to the error that ssh gives with no args
@@ -46,9 +55,18 @@ function ash() {
   else
     local target_host=$1
     local shortname=$(echo $LC_alias_of_target_host | sed -e 's/\..*//')
+    grep -qFx "$target_host" $KNOWN_HOSTS_FILE || echo $target_host >> $KNOWN_HOSTS_FILE
     LC_alias_of_target_host=$shortname ssh $target_host
   fi
 }
+# bash auto completion for host names
+function _ash_complete_options() {
+  local curr_arg=${COMP_WORDS[COMP_CWORD]}
+  local lines
+  IFS=$'\n' read -d '' -r -a lines < $KNOWN_HOSTS_FILE
+  COMPREPLY=( $(compgen -W '${lines[@]}' -- $curr_arg ) )
+}
+complete -F _ash_complete_options ash
 
 # Gets the hostname entered when ssh-ing to the machine.
 function getExpectedHostname() {
