09ebe50c27ed342ddd97fbf8c3832fb72c06fbfd
Joe Kearney <jjk@soundcloud.com>
2015-11-17 19:27:16 +0100

    add .screenrc

--- linkDotFiles.sh
+++ linkDotFiles.sh
@@ -2,7 +2,7 @@
 
 DOT_FILES_DIR=$(dirname $0)
 
-for f in .bashrc .bash_profile .vimrc .gitconfig; do
+for f in .bashrc .bash_profile .vimrc .gitconfig .screenrc; do
 	ln -sf $DOT_FILES_DIR/$f ~/$(basename $f)
 done
 
