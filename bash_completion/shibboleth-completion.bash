d321292ed4054ac60bc2f8082da4f8c18fc797cf
Joe Kearney <jjk@soundcloud.com>
2015-11-24 10:16:55 +0100

    fix shibboleth completion, 2nd arg is filenames

--- bash_completion/shibboleth-completion.bash
+++ bash_completion/shibboleth-completion.bash
@@ -1,7 +1,11 @@
 # bash auto completion for shibboleth
 function _shibboleth_complete_options() {
   local curr_arg=${COMP_WORDS[COMP_CWORD]}
-  local lines=$(echo "show|edit|diff|secure|version|help" | tr '|' '\n')
-  COMPREPLY=( $(compgen -W '${lines[@]}' -- $curr_arg ) )
+  if [[ "$COMP_CWORD" == "1" ]]; then
+    local lines=$(echo "show|edit|diff|secure|version|help" | tr '|' '\n')
+    COMPREPLY=( $(compgen -W '${lines[@]}' -- $curr_arg ) )
+  else
+    COMPREPLY=( $(compgen -f -- $curr_arg ) )
+  fi
 }
-complete -F _shibboleth_complete_options shibboleth
+complete -F _shibboleth_complete_options -o filenames shibboleth
