1018677e4283969a7ee8f1028094a135e4517fe9
Joe Kearney <jjk@soundcloud.com>
2016-01-14 23:26:29 +0100

    network test tweaks, mostly reporting

--- bin/network-test.sh
+++ bin/network-test.sh
@@ -3,11 +3,6 @@
 TARGETS=$(cat $DOT_FILES_DIR/big-websites.txt)
 NUM_TARGETS=$(echo "$TARGETS" | wc -l)
 
-echo "Testing network connection using $NUM_TARGETS targets..."
-echo
-
-PROBLEM_TESTS=()
-
 function runTest() {
   local name=$1
   local op=$2
@@ -18,34 +13,59 @@ function runTest() {
     output=$($op $w 2>&1)
     result=$?
     if [ $result -ne 0 ]; then
-      failed=1
+      ((failed=failed+1))
       echo " + Failed test [$name] on [$w], with exit code [$result]"
       echo "$output" | sed 's/^/ |  /'
     fi
   done
 
-  if [ $failed -eq 1 ]; then
-    PROBLEM_TESTS+=("$name")
+  if [ $failed -gt 0 ]; then
+    PROBLEM_TESTS+=("$name (failed $failed)")
+  fi
+
+  return $failed
+}
+
+function runTestSuite() {
+  local PING_TIMEOUT=1
+  local DNS_TIMEOUT=1
+  local DNS_RETRIES=1
+  local HTTP_TIMEOUT=1
+
+  echo "Testing network connection using $NUM_TARGETS targets..."
+  echo
+
+  local PROBLEM_TESTS=()
+
+  runTest "dns lookup over udp" "host -W $DNS_TIMEOUT -R $DNS_RETRIES"
+  runTest "dns lookup over tcp" "host -W $DNS_TIMEOUT -R $DNS_RETRIES -T"
+  runTest "ping" "ping -oq -t $PING_TIMEOUT"
+  runTest "http" "http --headers --timeout $HTTP_TIMEOUT"
+
+  echo
+  echo "All tests completed"
+  if [ "${#PROBLEM_TESTS[@]}" -gt 1 ]; then
+    echo "There were problems in these tests:"
+    for p in "${PROBLEM_TESTS[@]}"; do
+      echo "  $p"
+    done
+    exit 1
+  else
+    exit
   fi
 }
 
-PING_TIMEOUT=2
-DNS_TIMEOUT=1
-DNS_RETRIES=1
+function runUntilFailed() {
+  local keepGoing=1
 
-runTest "dns lookup over udp" "host -W $DNS_TIMEOUT -R $DNS_RETRIES"
-runTest "dns lookup over tcp" "host -W $DNS_TIMEOUT -R $DNS_RETRIES -T"
-runTest "ping" "ping -oq -t $PING_TIMEOUT"
-runTest "http" "http --headers"
+  if [ $keepGoing -gt 0 ]; then
+#    echo running
+#    keepGoing=runTestSuite
+  fi
+}
 
-echo
-echo "All tests completed"
-if [ "${#PROBLEM_TESTS[@]}" -gt 1 ]; then
-  echo "There were problems in these tests:"
-  for p in "${PROBLEM_TESTS[@]}"; do
-    echo "  $p"
-  done
-  exit 1
+if [[ "$1" == "-r" ]]; then
+  runUntilFailed
 else
-  exit
+  runTestSuite
 fi
