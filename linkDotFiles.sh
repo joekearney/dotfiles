f9023c49b2ba3769b1531981319cfbc6bc6b65df
Joe Kearney <jjk@soundcloud.com>
2018-09-03 16:42:39 +0100

    automatic linking of dropbox-based local env file

--- linkDotFiles.sh
+++ linkDotFiles.sh
@@ -6,4 +6,9 @@ for f in .bashrc .bash_profile .vimrc git/.gitconfig .screenrc .inputrc; do
 	ln -sf $DOT_FILES_DIR/$f ~/$(basename $f)
 done
 
+DROPBOX_LOCAL_ENV="~/Dropbox/.local_env.sh"
+if [[ -f "${DROPBOX_LOCAL_ENV}" ]]; then
+	ln -s "${DROPBOX_LOCAL_ENV}" $(basename "${DROPBOX_LOCAL_ENV}")
+fi
+
 echo "If you need to commit to git, remember to update your email in $HOME/.config/git/config, in a [user].email property."
