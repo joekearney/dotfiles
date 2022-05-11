949316a06b97a6a9cc935d5c828d22ee8b057e29
Joe Kearney <mail@joekearney.co.uk>
2022-05-11 13:51:15 +0200

    Add colourful man pages

--- bash/bash-functions.sh
+++ bash/bash-functions.sh
@@ -285,3 +285,15 @@ function run-updates() {
   maybeRunUpdate "npm" "npm upgrade -g" # update is a synonym
   maybeRunUpdate "mas" "mas upgrade"
 }
+
+function man() {
+    env \
+        LESS_TERMCAP_mb="$(printf "\e[1;31m")" \
+        LESS_TERMCAP_md="$(printf "\e[1;31m")" \
+        LESS_TERMCAP_me="$(printf "\e[0m")" \
+        LESS_TERMCAP_se="$(printf "\e[0m")" \
+        LESS_TERMCAP_so="$(printf "\e[1;44;33m")" \
+        LESS_TERMCAP_ue="$(printf "\e[0m")" \
+        LESS_TERMCAP_us="$(printf "\e[1;32m")" \
+            man "$@"
+}
